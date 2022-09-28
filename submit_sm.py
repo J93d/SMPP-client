331bf13414e8a170471912b11a6f6ff7b91b2d29
J93d <joydebskms@gmail.com>
2022-09-28 12:33:26 +0530

    modified:   submit_sm.py

--- submit_sm.py
+++ submit_sm.py
@@ -2,8 +2,6 @@
 
 from struct import pack
 from random import randint
-import os
-import sys
 from textwrap import wrap
 
 def submit_sm(socket):
@@ -102,54 +100,69 @@ def submit_sm(socket):
         sm_default_msg_id=pack(">B",0)
         
     msg=input('Enter your message: ')
-    
-    if len(msg)<=255:
-    #if len(msg)<=150:
-        sequence_number=randint(1,65536)
-        short_message=msg.encode()
-        sm_length=pack(">B",len(short_message))
-        command_length=33+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
-        data=pack('!4I',command_length,command_id,command_status,sequence_number)
-        socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+short_message)
-    elif len(msg)>255:
-    #elif len(msg)>150:
-        #esm_class=pack(">B",64)
-        sequence_number=randint(1,65536)        
-        #temp_msg_wrapd=wrap(msg,width=150)
-        temp_msg_wrapd=wrap(msg,width=255)
-        if len(temp_msg_wrapd)>255:
-            print('Too Long Message')
-            return None
-        sar_seq=randint(1,255)
-        #udh_length=pack(">B",5)
-        #ieid=pack(">H",3)
-        #msg_identifier=pack(">B",randint(0,255))
-        for i in range(0,len(temp_msg_wrapd)):
-            short_message=temp_msg_wrapd[i].encode()
-            #sm_length=pack(">B",(len(short_message)+int(6)))
-            sm_length=pack(">B",(len(short_message)))
-            ####################################optional parameters################################
-            #sar_msg_ref_num
-            tag1=pack(">H",524)
-            length1=pack(">H",2)
-            ref1=pack(">H",sar_seq)
-            sar_msg_ref_num=(tag1+length1+ref1)
-            #sar_segment_seqnum
-            tag2=pack(">H",527)
-            length2=pack(">H",1)
-            seq2=pack(">B",(i+1))
-            sar_segment_seqnum=(tag2+length2+seq2)
-            #sar_total_segments
-            tag3=pack(">H",526)
-            length3=pack(">H",1)
-            size3=pack(">B",len(temp_msg_wrapd))
-            sar_total_segments=(tag3+length3+size3)
-            
-            #msg_parts=pack(">B",len(temp_msg_wrapd))
-            #msg_part_num=pack(">B",(i+1))
-            
-            #command_length=39+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
-            command_length=49+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
+    if e_class:                                                         #This is for UDH message
+        if len(msg)<=150:
+            sequence_number=randint(1,65536)
+            short_message=msg.encode()
+            sm_length=pack(">B",len(short_message))
+            command_length=33+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
             data=pack('!4I',command_length,command_id,command_status,sequence_number)
-            #socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+udh_length+ieid+msg_identifier+msg_parts+msg_part_num+short_message)#+sar_msg_ref_num+sar_segment_seqnum+sar_total_segments)
-            socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+short_message+sar_msg_ref_num+sar_segment_seqnum+sar_total_segments)
+            socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+short_message)
+        elif len(msg)>150:
+            esm_class=pack(">B",64)       
+            temp_msg_wrapd=wrap(msg,width=150)
+            if len(temp_msg_wrapd)>255:
+                print('Too Long Message')
+                return None
+            udh_length=pack(">B",5)
+            ieid=pack(">H",3)
+            msg_identifier=pack(">B",randint(0,255))
+            for i in range(0,len(temp_msg_wrapd)):
+                short_message=temp_msg_wrapd[i].encode()
+                sm_length=pack(">B",(len(short_message)+int(6)))
+
+                msg_parts=pack(">B",len(temp_msg_wrapd))
+                msg_part_num=pack(">B",(i+1))
+
+                command_length=39+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
+                data=pack('!4I',command_length,command_id,command_status,sequence_number)
+                socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+udh_length+ieid+msg_identifier+msg_parts+msg_part_num+short_message)
+                
+    else:                                                         #This is for SAR message
+        if len(msg)<=255:
+            sequence_number=randint(1,65536)
+            short_message=msg.encode()
+            sm_length=pack(">B",len(short_message))
+            command_length=33+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
+            data=pack('!4I',command_length,command_id,command_status,sequence_number)
+            socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+short_message)
+        elif len(msg)>255:
+            sequence_number=randint(1,65536)
+            temp_msg_wrapd=wrap(msg,width=255)
+            if len(temp_msg_wrapd)>255:
+                print('Too Long Message')
+                return None
+            sar_seq=randint(1,255)
+            for i in range(0,len(temp_msg_wrapd)):
+                short_message=temp_msg_wrapd[i].encode()
+                sm_length=pack(">B",(len(short_message)))
+                ####################################optional parameters################################
+                #sar_msg_ref_num
+                tag1=pack(">H",524)
+                length1=pack(">H",2)
+                ref1=pack(">H",sar_seq)
+                sar_msg_ref_num=(tag1+length1+ref1)
+                #sar_segment_seqnum
+                tag2=pack(">H",527)
+                length2=pack(">H",1)
+                seq2=pack(">B",(i+1))
+                sar_segment_seqnum=(tag2+length2+seq2)
+                #sar_total_segments
+                tag3=pack(">H",526)
+                length3=pack(">H",1)
+                size3=pack(">B",len(temp_msg_wrapd))
+                sar_total_segments=(tag3+length3+size3)
+
+                command_length=49+len(service_type)+len(source_addr)+len(destination_addr)+len(schedule_delivery_time)+len(validity_period)+len(short_message)
+                data=pack('!4I',command_length,command_id,command_status,sequence_number)
+                socket.send(data+service_type+b'\x00'+source_addr_ton+source_addr_npi+source_addr+b'\x00'+dest_addr_ton+dest_addr_npi+destination_addr+b'\x00'+esm_class+protocol_id+priority_flag+schedule_delivery_time+b'\x00'+validity_period+b'\x00'+registered_delivery+replace_if_present_flag+data_coding+sm_default_msg_id+sm_length+short_message+sar_msg_ref_num+sar_segment_seqnum+sar_total_segments)        
